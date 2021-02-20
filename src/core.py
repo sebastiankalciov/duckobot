4b1ad31df787a39a29ee8f8549961c3ac7b5dce1
Kali <zduck1337@gmail.com>
2021-02-20 11:09:18 -0500

    Fixed token, cogs issues.

--- src/core.py
+++ src/core.py
@@ -1,21 +1,30 @@
 import glob
 from discord.ext import commands
 import discord
-
+import json
+import platform
 bot = commands.Bot(command_prefix='-') # Initiating a bot with a custom prefix.
 bot.remove_command('help') # Removing the help command to add a custom one.
 
 import os, sys
 from os.path import dirname, join, abspath
 sys.path.insert(0, abspath(join(dirname(__file__), '..')))
+
 #from Data.Functions.reloadFunction import reload
-from Data.JSON import config
+
+
+data = json.load(open('./Data/JSON/config.json', "r"))
+token = data["token"]
+
+
 @bot.event
 
 async def on_ready(): # On ready event
 
     print(f'Ready <{bot.user.name}>')
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="America"))
+
+
 
 @bot.event
 
@@ -48,21 +57,23 @@ if __name__ == "__main__": # Running cogs
 
     for file in possibleCommands:
 
-
         try:
-            
+                        
+            if file.endswith('.py'):    
+                
+                if platform.system() == "Linux":
 
+                    bot.load_extension(file[2:].replace('/', '.')[:-3])
 
-
-            file.endswith('.py'):    
-
-                bot.load_extension(file[2:].replace('\\', '.')[:-3])
-                print("Cog pornit: " + str(file[2:].replace('\\', '.')[:-3].split(".")[2]))
+                    print("Cog stable: " + str(file[2:].replace('/', '.')[:-3].split(".")[2]))
+                else:
+                    bot.load_extension(file[2:].replace('\\', ".")[:3])
+                    print("Cog stable: " + str(file[2:].replace("\\", ".")[:-3].split(".")[2]))
 
         except Exception as e:
 
             exc = '{}: {}'.format(type(e).__name__, e)
-            print('Eroare, cog problema: {}\n{}'.format(file, exc))
+            print('Error, unstable cog: {}\n{}'.format(file, exc))
 
 
 #@bot.command()
@@ -70,4 +81,4 @@ if __name__ == "__main__": # Running cogs
 #    await reload(self, ctx)
 
 
-bot.run(config.token)
+    bot.run(token)

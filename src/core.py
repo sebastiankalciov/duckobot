1a1dbe0fa118e775b980624ef1aff6912b5176eb
Sebastian Kalciov <66016734+ducktheduck@users.noreply.github.com>
2021-02-21 02:11:51 +0200

    Fixed major issues in security.

--- src/core.py
+++ src/core.py
@@ -10,7 +10,6 @@ bot.remove_command('help') # Removing the help command to add a custom one.
 # from os.path import dirname, join, abspath
 # sys.path.insert(0, abspath(join(dirname(__file__), '..')))
 
-
 data = json.load(open('./Data/JSON/config.json', "r"))
 token = data["token"]
 
